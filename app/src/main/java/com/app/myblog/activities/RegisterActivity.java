64458c08219089998fcdf62e2637e0660a7d3d94
Takahiro Tanaka <t_tkhr@icloud.com>
2021-05-12 21:13:42 +0900

    Firebase設定 & 登録画面のレイアウト作成

---
+++ app/src/main/java/com/app/myblog/activities/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.app.myblog.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.myblog.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
