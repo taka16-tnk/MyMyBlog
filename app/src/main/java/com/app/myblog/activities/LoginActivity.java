738e726ebdb19fc8f13409840feed957f5079e35
Takahiro Tanaka <t_tkhr@icloud.com>
2021-06-01 23:19:49 +0900

    Firebaseログイン認証の実装

---
+++ app/src/main/java/com/app/myblog/activities/LoginActivity.java
@@ -0,0 +1,114 @@
+package com.app.myblog.activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.app.myblog.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText userMail, userPassword;
+    private Button loginBtn;
+    private ProgressBar loginProgress;
+    private FirebaseAuth mAuth;
+    private Intent HomeActivity;
+    private ImageView loginPhoto;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        userMail = findViewById(R.id.login_email);
+        userPassword = findViewById(R.id.login_password);
+        loginBtn = findViewById(R.id.login_btn);
+        loginProgress = findViewById(R.id.login_progress);
+        mAuth = FirebaseAuth.getInstance();
+        HomeActivity = new Intent(this, com.app.myblog.activities.HomeActivity.class);
+        loginPhoto = findViewById(R.id.login_photo);
+        loginPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerActivity = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(registerActivity);
+                finish();
+            }
+        });
+
+        loginProgress.setVisibility(View.INVISIBLE);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                loginProgress.setVisibility(View.VISIBLE);
+                loginBtn.setVisibility(View.INVISIBLE);
+
+                final String mail = userMail.getText().toString();
+                final String password = userPassword.getText().toString();
+
+                if (mail.isEmpty() || password.isEmpty()) {
+                    showMessage("Please Verify All Field");
+                    loginBtn.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                } else {
+                    signIn(mail, password);
+                }
+            }
+        });
+    }
+
+    private void signIn(String mail, String password) {
+
+        mAuth.signInWithEmailAndPassword(mail, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    loginProgress.setVisibility(View.INVISIBLE);
+                    loginBtn.setVisibility(View.VISIBLE);
+                    updateUI();
+                } else {
+                    showMessage(task.getException().getMessage());
+                    loginBtn.setVisibility(View.VISIBLE);
+                    loginProgress.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+    }
+
+    private void updateUI() {
+        startActivity(HomeActivity);
+        finish();
+    }
+
+    private void showMessage(String text) {
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            // ホーム画面にリダイレクトする
+            updateUI();
+        }
+    }
+}
